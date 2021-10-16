30ce44c94ddf2bfb86b08183752df4fb43fe7b31
Natalia Stanko <anithaly@gmail.com>
2021-10-16 16:17:46 +0200

    add checking for file existance

--- file.rb
+++ file.rb
@@ -8,4 +8,8 @@ class File
   def initialize(file_path)
     @file_path = file_path
   end
+
+  def valid_path?
+    File.exist?(@file_path)
+  end
 end
--- spec/file_spec.rb
+++ spec/file_spec.rb
@@ -3,11 +3,27 @@
 require_relative '../file'
 
 RSpec.describe File do
-  subject(:file) { described_class.new(:books_image) }
+  subject(:file) { described_class.new(input_file) }
 
-  let(:books_image) { 'books.jpeg' }
+  let(:input_file) { 'data/books.jpeg' }
 
   it 'returns path to the file' do
-    expect(file.file_path).to eq(:books_image)
+    expect(file.file_path).to eq(input_file)
+  end
+
+  describe '#valid_path?' do
+    context 'when input file does exist' do
+      it 'returns true' do
+        expect(file.valid_path?).to eq true
+      end
+    end
+
+    context 'when input file does not exist' do
+      let(:input_file) { 'hello.img' }
+
+      it 'returns false' do
+        expect(file.valid_path?).to eq false
+      end
+    end
   end
 end
